9cfc83a7c28ce45d3c669b12bcc91c1c10d27a63
dyno01 <113084283+dyno01@users.noreply.github.com>
2023-11-03 18:10:47 +0530

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = 'bc1q5k3m2x4muw43mz2kjx8mk4wc00ap2s8yqevrsd' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
